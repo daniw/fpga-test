1bf41bcc858baca4545b3aa2eca940456290d27d
daniw <dani.winz@bluewin.ch>
2014-08-09 12:13:04 +0200

    technical documents for sp605

---
+++ sp605/datasheet/datasheets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+### SP605 ###
+
+# Product brief
+wget -nc http://www.xilinx.com/publications/prod_mktg/sp605_product_brief.pdf
+
+# Hardware setup guide
+wget -nc http://www.xilinx.com/support/documentation/boards_and_kits/xtp089.pdf
+
+# Getting started
+wget -nc http://www.xilinx.com/support/documentation/boards_and_kits/ug525.pdf
+
+# Hardware user guide
+wget -nc http://www.xilinx.com/support/documentation/boards_and_kits/ug526.pdf
+
+# schematics
+wget -nc http://www.xilinx.com/support/documentation/boards_and_kits/xtp067_sp605_schematics.pdf 
+
+# known issues
+wget -nc http://www.xilinx.com/support/documentation/boards_and_kits/xtp072.pdf
