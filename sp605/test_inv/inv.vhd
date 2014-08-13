77291cdde64cf100c2c0e36b223a4192fb3ec861
daniw <dani.winz@bluewin.ch>
2014-08-13 15:58:10 +0200

    Comment removed

--- sp605/test_inv/inv.vhd
+++ sp605/test_inv/inv.vhd
@@ -1,33 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    23:19:05 08/08/2014 
--- Design Name: 
--- Module Name:    inv - arch_inv 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity inv is
     Port ( a : in  STD_LOGIC;
